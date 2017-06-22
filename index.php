a18b02969111fe20ade07a6a0999c95bb2fde641
Ruben Philipp <rp@rubylon.de>
2017-06-22 16:06:27 +0200

    added Skeleton

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Page Title</title>
+  <meta name="description" content="">
+  <meta name="author" content="Ruben Philipp">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="dist/styles/normalize.css">
+  <link rel="stylesheet" href="dist/styles/skeleton.css">
+
+  <link rel="stylesheet" href="dist/styles/main.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <!--<link rel="icon" type="image/png" href="dist/images/favicon.png">-->
+
+</head>
+<body>
+
+
+</body>
+</html>
