d5ae04b31efbe125856074670d89056f0ca65646
Ruben Philipp <rp@rubylon.de>
2017-06-26 16:37:47 +0200

    implemented aligner as a solution for vertical align

--- index.php
+++ index.php
@@ -29,9 +29,11 @@
       <header style="text-align: right; padding-right: 20px;">
         <a href="http://github.com/rubenphilipp/basic-boilerplate" title="view source on GitHub">view source on GitHub</a>
       </header>
-      <div class="content" style="padding-top: 25vh;">
-        <h1>Let the journey start…</h1>
-        <h2>…right now</h2>
+      <div class="content u-aligner">
+        <div class="u-aligner-item">
+          <h1>Let the journey start…</h1>
+          <h2>…right now</h2>
+        </div>
       </div>
       <footer style="text-align: center;">
         <p>created by <i>Ruben Philipp</i></p>
